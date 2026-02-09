02b5deb8408af9af3f88b252cdbe77d5849f6a6f
hccccrr <vrajiv830@gmail.com>
2026-02-09 19:07:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
-import asyncio
-import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+import asyncio
 
-TOKEN = "8263898247:AAEjfKb2d8PdGLefAVBk7TdHtm0Q81Cot8o"
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=TOKEN)
+bot = Bot("8263898247:AAEjfKb2d8PdGLefAVBk7TdHtm0Q81Cot8o")
 dp = Dispatcher()
-
 
 @dp.message(Command("start"))
 async def start_cmd(message: types.Message):
-    text = (
-        "🔥 Test Start Message\n\n"
-        "This is a premium emoji test:\n"
-        "<tg-emoji emoji-id=\"5298727558711629080\">🔥</tg-emoji>\n\n"
-        "If you see animation → Premium ✅\n"
-        "If not → Normal user 🙂"
+    await message.answer(
+        'Test 🔥 <tg-emoji emoji-id="5298727558711629080">🔥</tg-emoji>',
+        parse_mode="HTML"
     )
-    await message.answer(text, parse_mode="HTML")
-
 
 async def main():
     await dp.start_polling(bot)
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
